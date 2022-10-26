8a605d4d7ee08896dc6932b251cdc1804ba203f5
thkim1 <thkim1@douzone.com>
2022-10-26 19:47:35 +0900

    1026 5,8

--- src/String/String_8.java
+++ src/String/String_8.java
@@ -7,29 +7,11 @@ public class String_8 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		String []str = reader.readLine().toLowerCase().replaceAll("[^\\sa-zA-Z]", "").split(" ");
+		String str = reader.readLine().toLowerCase().replaceAll("[^a-z]", "");
+		String result;
 		
-		for(int i = 0; i < str.length; i++) {
-			if(!(sb.append(str[i]).reverse().toString().equals(str[str.length - i - 1]))) {
-				System.out.println("NO");
-				return ;
-			} else {
-				sb.setLength(0);
-			}
-		}
+		result = str.equals(sb.append(str).reverse().toString()) ? "YES" : "NO";
 		
-//		found7, time: study; Yduts; emit, 7Dnuof
-		
-		
-//		for(String s : str.split(" ")) {
-//			if(!str.contains(sb.append(s).reverse())) {
-//				System.out.println("NO");
-//				return ;
-//			} else {
-//				sb.setLength(0);
-//			}
-//		}
-		System.out.println("YES");
-		
+		System.out.println(result);
 	}
 }
